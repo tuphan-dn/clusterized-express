793a030f342493fa6e5d29c00e820c80ae53e9a1
tuphan-dn <tuphan@descartes.network>
2024-09-08 14:12:30 +0700

    add benchmark

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# clusterized-express
+# Clusterized Express
+
+## Start the service
+
+```bash
+pnpm install
+pnpm dev
+```
+
+## Run the benchmark
+
+```bash
+pnpm test
+```
--- index.ts
+++ index.ts
@@ -1,12 +1,26 @@
 import express from 'express'
+import cluster from 'cluster'
+import process from 'node:process'
 
 const app = express()
-const port = 3000
+const cpus = 4
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log('⚡️ Listening on port', port)
-})
+// app.listen(process.env.PORT, () => {
+//   console.log(`👷 Worker listening on port ${process.env.PORT}`)
+// })
+
+if (cluster.isPrimary) {
+  console.log(`⚡️ Primary listening on port ${process.env.PORT}`)
+  for (let i = 0; i < cpus; i++) cluster.fork()
+  cluster.on('exit', (worker) => {
+    console.log(`Worker ${worker.process.pid} died`)
+  })
+} else {
+  app.listen(process.env.PORT, () => {
+    console.log(`👷 Worker listening on pid ${process.pid}`)
+  })
+}
